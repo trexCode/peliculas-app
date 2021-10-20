6412da4e9260125743604092ee6b14ad9870db11
trexcode <ingldurandaniel@gmail.com>
2021-10-20 14:11:19 -0500

    Card Swiper dependency and card_swiper widget was added

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:peliculas_app/screens/screens.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
 
   @override
@@ -14,10 +13,14 @@ class MyApp extends StatelessWidget {
       title: 'Peliculas',
       initialRoute: 'home',
       routes: {
-        'home': ( _ ) => const HomeScreen(),
-        'details': ( _ ) => const DetailsScreen()
+        'home': (_) => const HomeScreen(),
+        'details': (_) => const DetailsScreen()
       },
-      
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Colors.indigo,
+        ),
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Películas en cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search_outlined),
+          ),
+        ],
+      ),
+      body: Column(
+        children: const [
+          CardSwiper(),
+        ],
       ),
     );
   }
